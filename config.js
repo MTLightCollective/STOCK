fa0005f4960f40809b7802f98e4305ff2e5b8b73
MTLight Matt <50854957+MTLightCollective@users.noreply.github.com>
2024-09-12 16:59:36 -0400

    Add files via upload
    adding alphavantage API key

---
+++ config.js
@@ -0,0 +1,7 @@
+// Configuration file for API keys and other sensitive information
+const config = {
+    // Alpha Vantage API key
+    alphavantageApiKey: '1MF89VK3UMB3YNR3',
+
+    // You can add other API keys or configuration options here as needed
+};

407d9c74e909e4eaf4a203cd0c5aff3d2f9a3a8e
MTLight Matt <50854957+MTLightCollective@users.noreply.github.com>
2024-09-13 09:16:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 const config = {
     // Alpha Vantage API key
-    alphavantageApiKey: '1MF89VK3UMB3YNR3'
-    // other alphavantageApiKey: 'D0UO0BF3XHJSTHR8'
+    // alphavantageApiKey: '1MF89VK3UMB3YNR3'
+    alphavantageApiKey: 'D0UO0BF3XHJSTHR8'
     //fmpApiKey: 'pJWbg910AMpiLpMQ2Wl3mBTgkIsHC9uC'
     // You can add other API keys or configuration options here as needed
 };
